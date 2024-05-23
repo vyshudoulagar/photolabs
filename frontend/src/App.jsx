04a4d75cbeffe1cd838b0e2fc3421843b44d20e1
WishNavy <du.vyshu@gmail.com>
2024-05-23 15:16:00 -0700

    add home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 
@@ -16,8 +14,7 @@ const App = () => {
                     <PhotoListItem data={sampleDataForPhotoListItem} />
                 </div>
             ))} */}
-            <TopNavigation />
-            <PhotoList />
+            <HomeRoute />
         </div>
     );
 };
