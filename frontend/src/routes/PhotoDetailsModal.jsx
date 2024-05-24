50ef87aeb41c041ec4aaf8f6d3af98970bcd0aa4
WishNavy <du.vyshu@gmail.com>
2024-05-24 16:55:11 -0700

    add similar photos to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,26 +3,35 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
+import photos from 'mocks/photos';
 
-const PhotoDetailsModal = ({ onClick, displayModal , setDisplayModal }) => {
-  return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={() => setDisplayModal(null)}>
-        <img src={closeSymbol} alt="close symbol" />
-        <PhotoFavButton photo={displayModal.id} onClick={onClick}/>
-            <img className="photo-list__image" src={displayModal.urls.regular} alt={displayModal.location.city} />
-            <div className="photo-list__user-details">
-                <img className="photo-list__user-profile" src={displayModal.user.profile} alt={displayModal.user.username} />
-                <div className="photo-list__user-info">
+const PhotoDetailsModal = ({ onClick, displayModal, setDisplayModal }) => {
+    console.log(displayModal);
+    return (
+        <div className="photo-details-modal">
+            <button className="photo-details-modal__close-button" onClick={() => setDisplayModal(null)}>
+                <img src={closeSymbol} alt="close symbol" />
+            </button>
+            <PhotoFavButton photo={displayModal.id} onClick={onClick} />
+            <img className="photo-details-modal__image" src={displayModal.urls.regular} alt={displayModal.location.city} />
+            <div className="photo-details-modal__photographer-details">
+                <img className="photo-details-modal__photographer-profile" src={displayModal.user.profile} alt={displayModal.user.username} />
+                <div className="photo-details-modal__photographer-info">
                     <p>{displayModal.user.username}</p>
-                    <div className="photo-list__user-location">
+                    <div className="photo-details-modal__photographer-location">
                         <p>{displayModal.location.city} {displayModal.location.country}</p>
                     </div>
                 </div>
             </div>
-      </button>
-    </div>
-  )
+            <div className='photo-details-modal__header'>
+                <p>Similar Photos</p>
+                <div className='photo-details-modal__images'>
+                    <PhotoList photos={photos} onClick={onClick} setDisplayModal={setDisplayModal} />
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default PhotoDetailsModal;
