139e5f2d4a5671ba65e3021d72ed4ae99207db23
WishNavy <du.vyshu@gmail.com>
2024-05-24 12:11:34 -0700

    add details to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -8,6 +8,17 @@ const PhotoDetailsModal = ({ setDisplayModal }) => {
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={() => setDisplayModal(false)}>
         <img src={closeSymbol} alt="close symbol" />
+        <PhotoFavButton photo={data.id} onClick={onClick}/>
+            <img className="photo-list__image" src={data.urls.regular} alt={data.location.city} />
+            <div className="photo-list__user-details">
+                <img className="photo-list__user-profile" src={data.user.profile} alt={data.user.username} />
+                <div className="photo-list__user-info">
+                    <p>{data.user.username}</p>
+                    <div className="photo-list__user-location">
+                        <p>{data.location.city} {data.location.country}</p>
+                    </div>
+                </div>
+            </div>
       </button>
     </div>
   )
