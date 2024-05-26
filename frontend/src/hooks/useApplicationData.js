777109d89ddddb99d61dd4033a53de89373054a2
WishNavy <du.vyshu@gmail.com>
2024-05-26 15:54:21 -0700

    add useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,35 +1,68 @@
-import { useState } from "react";
+
+import { useReducer } from "react";
+
+export const ACTIONS = {
+    FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+    FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+    SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+    SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+    SELECT_PHOTO: 'SELECT_PHOTO',
+    DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.FAV_PHOTO_ADDED:
+            return {
+                ...state,
+                favPhoto: state.favPhoto.includes(action.payload.id)
+                    ? state.favPhoto
+                    : [...state.favPhoto, action.payload.id]
+            };
+        case ACTIONS.FAV_PHOTO_REMOVED:
+            return {
+                ...state,
+                favPhoto: state.favPhoto.filter(id => id !== action.payload.id)
+            };
+        case ACTIONS.SELECT_PHOTO:
+            return {
+                ...state,
+                displayModal: action.payload.photo
+            };
+        case ACTIONS.DISPLAY_PHOTO_DETAILS:
+            return {
+                ...state,
+                displayModal: action.payload.photoDetails,
+            };
+        default:
+            throw new Error(
+                `Tried to reduce with unsupported action type: ${action.type}`
+            );
+    }
+}
 
 const useApplicationData = () => {
-    const [state, setState] = useState({
+    const initialState = {
         favPhoto: [],
         displayModal: null
-    });
+    }
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     const updateToFavPhotoIds = (id) => {
-        setState(prevState => ({
-            ...prevState,
-            favPhoto: prevState.favPhoto.includes(id)
-                ? prevState.favPhoto.filter(photoId => photoId !== id)
-                : [...prevState.favPhoto, id]
-        }));
+        state.favPhoto.includes(id)
+            ? dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, payload: { id } })
+            : dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, payload: { id } });
     };
 
     const onPhotoSelect = (photo) => {
-        setState((prevState) => ({
-            ...prevState,
-            displayModal: photo
-        }));
+        dispatch({ type: ACTIONS.SELECT_PHOTO, payload: { photo } });
     };
 
     const onClosePhotoDetailsModal = () => {
-        setState((prevState) => ({
-            ...prevState,
-            displayModal: null
-        }));
-
+        dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS, payload: { photoDetails: null } });
     };
-    return {state, updateToFavPhotoIds, onPhotoSelect, onClosePhotoDetailsModal};
+    
+    return { state, updateToFavPhotoIds, onPhotoSelect, onClosePhotoDetailsModal };
 }
 
 export default useApplicationData;
